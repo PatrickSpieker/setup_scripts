0411d15154d43ed410c2f4465848d7dcaa682a24
patrick <patrick@patrickspieker.com>
2022-02-19 15:36:22 -0800

    WIP

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 # This file exists both for literal config purposes (to programmically redo my setup)
 # and also for intellectual purposes - to remind me how my setup works + tips and tricks
@@ -15,8 +15,10 @@ echo "Adding new bash to list of allowable shells..."
 echo "/usr/local/bin/bash" | sudo tee -a /etc/shells >> /dev/null # adding the upgraded bash to our shell options
 echo "Making the system shell the new bash..."
 chsh -s /usr/local/bin/bash # actually changing the shell now that the new bash is an option
+# Soft linking the bashrc, then creating bash_profile such that it just references rc - all config should be in rc
+ln -sf /Users/patrickspieker/Library/Mobile\ Documents/com~apple~CloudDocs/setup_scripts/bashrc_main ~/.bashrc
+echo "source ~/.bashrc" > ~/.bash_profile
 echo "Finished configuing new bash!"
-
 
 brew install tmux # for terminal multi-plexing
 brew install fzf # file fuzzy finder - mostly used in the nvim context
@@ -34,7 +36,8 @@ git config --global user.name "pspieker"
 brew install neovim # for most text editing needs
 # Softlinking the NVIM config in this folder to the correct file system location
 # Note that the _entire_ file path is needed here - 
-ln -s /Users/patrickspieker/Library/Mobile\ Documents/com~apple~CloudDocs/setup_scripts/vimrc_main ~/.config/nvim/init.vim
+# (should set up a bash variable to store the path to this location)
+ln -sf /Users/patrickspieker/Library/Mobile\ Documents/com~apple~CloudDocs/setup_scripts/vimrc_main ~/.config/nvim/init.vim
 cp ./vim_config ~/.vimrc
 
 if [ -d "/Users/patrickspieker/.config/nvim" ] 
@@ -44,13 +47,12 @@ else
 fi
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# -------- END VIM CONFIG --------
 
-# Soft linking up config files - so the "correct" locations will point to the files here, in the repo
-ln -s ~/.config/nvim/init.vim ./vimrc_main
-ln -s /Users/patrickspieker/Library/Mobile\ Documents/com~apple~CloudDocs/setup_scripts/bashrc_main ~/.bashrc
-echo "source ~/.bashrc" > ~/.bash_profile
-cp ./zsh_config ~/.zshrc
-cp ./tmux_config ~/.tmux.conf
-source ~/.zshrc
+# soft linking tmux config
+ln -sf /Users/patrickspieker/Library/Mobile\ Documents/com~apple~CloudDocs/setup_scripts/tmux_conf_main ~/.tmux.conf
 
+# this will - run the stuff in bash_profile, which sources bashrc, so this needs to be the last step. 
+# This helps you avoid having to start a new shell to actually have these changes take effect. 
+source ~/.bash_profile
 echo "Done!"
