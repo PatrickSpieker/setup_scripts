ebcfab45e2fc756bfc5262c8c61ecb586b091943
pspieker <patrick@patrickspieker.com>
2023-01-22 21:05:45 -0800

    WIP

--- setup.sh
+++ setup.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-# This file exists both for literal config purposes (to programmically redo my setup)
-# and also for intellectual purposes - to remind me how my setup works + tips and tricks
+# This file exists both for literal development environment config purposes (to programmically redo my setup)
+# and also for human config purposes :) - to remind me how my setup works + tips and tricks
+#
+# NOTE: need to mark this script as executable with: chmod 755 ./setup.sh
 
-# HOMEBREW Installation 
-# need to mark this script as executable with: chmod 755 ./setup.sh
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# ----- HOMEBREW Installation -----
 # xcode-select --install # Apple's Command Line Tools - needed for a bunch of random Homebrew things 
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# BASH INSTALLATION - changing shell to an upgraded version of bash 
+# ----- BASH INSTALLATION ----- 
+# - changing shell to an upgraded version of bash 
+
 # brew install bash # just need the basics of shell - no customization beyond that (who needs color schemes?)
 # brew info bash # need this to get the path of the installed bash executable
 
 # should probably check if the above command worked ^^ ??? check with $? or something
 # For M1's for example, should be /opt/homebrew/bin/bash, which itself is a symlink
 echo "Adding new bash to list of allowable shells..."
-echo "/path/to/bash" | sudo tee -a /etc/shells >> /dev/null # adding the upgraded bash to our shell options
+# echo "/path/to/bash" | sudo tee -a /etc/shells >> /dev/null # adding the upgraded bash to our shell options
 echo "Making the system shell the new bash..."
 chsh -s /usr/local/bin/bash # actually changing the shell now that the new bash is an option
 echo $BASH_VERSION
@@ -26,6 +29,7 @@ ln -sf /Users/patrickspieker/Library/Mobile\ Documents/com~apple~CloudDocs/setup
 echo "source ~/.bashrc" >> ~/.bash_profile
 echo "Finished configuing new bash!"
 
+# ----- INSTALLATION VIA HOMEBREW -----
 # brew install fzf # file fuzzy finder - mostly used in the nvim context
 # brew install ruby # Old version of Ruby is usually the default, so want to upgrade
 # brew install ffmpeg # for extracting / dealing with audio files, converting, etc. 
@@ -64,10 +68,6 @@ ln -sf /Users/patrickspieker/Library/Mobile\ Documents/com~apple~CloudDocs/setup
 # Appending with >> as opposed to overwriting with >
 echo "Finished configuing VS Code!"
 # -------- END VS CODE CONFIG ------
-
-
-# soft linking tmux config - don't think I use this or need it?
-# ln -sf /Users/patrickspieker/Library/Mobile\ Documents/com~apple~CloudDocs/setup_scripts/tmux_conf_main ~/.tmux.conf
 
 # this will - run the stuff in bash_profile, which sources bashrc, so this needs to be the last step. 
 # This helps you avoid having to start a new shell to actually have these changes take effect. 
