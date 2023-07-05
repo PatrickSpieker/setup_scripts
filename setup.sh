4e1c077d16024e8a2fce513a0c84de77b8ff1070
pspieker <patrick@patrickspieker.com>
2023-07-05 13:04:42 -0700

    WIP

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@
 # NOTE: 
 # When you clone this repo, you need to mark this script as executable with: chmod 755 ./setup.sh
 
+# First, you may need to create / add the new laptop SSH key with: 
+# ssh-keygen -b 4096 -t rsa
+
 # ----- HOMEBREW Installation -----
 # xcode-select --install # Apple's Command Line Tools - needed for a bunch of random Homebrew things 
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -18,14 +21,18 @@
 
 # should probably check if the above command worked ^^ ??? check with $? or something
 # For M1's for example, should be /opt/homebrew/bin/bash, which itself is a symlink
+# For non-M1, I discovered this with: readlink /usr/local/bin/bash
 echo "Adding new bash to list of allowable shells..."
 # echo "/path/to/bash" | sudo tee -a /etc/shells >> /dev/null # adding the upgraded bash to our shell options
 echo "Making the system shell the new bash..."
 chsh -s /usr/local/bin/bash # actually changing the shell now that the new bash is an option
 echo $BASH_VERSION
 
+# Installing Rust:
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 # Soft linking the bashrc, then creating bash_profile such that it just references rc - all config should be in rc
-ln -sf /Users/patrickspieker/Library/Mobile\ Documents/com~apple~CloudDocs/setup_scripts/bashrc_main ~/.bashrc
+ln -sf ./setup_scripts/bashrc_main ~/.bashrc
 # Appending with >> as opposed to overwriting with >
 echo "source ~/.bashrc" >> ~/.bash_profile
 echo "Finished configuing new bash!"
@@ -39,6 +46,7 @@ echo "Finished configuing new bash!"
 # brew install yt-dlp/taps/yt-dlp # faster version of youtube download
 # brew install --cask emacs # thinking of trying this
 # brew install watchman # for sorbet
+# brew install neovim
 
 # Configuring personal git things
 git config --global user.email "patrick@patrickspieker.com"
@@ -48,7 +56,6 @@ git config --global user.name "pspieker"
 #
 # NEOVIM Installation
 # brew install neovim # for most text editing needs
-
 if [ -d "/Users/patrickspieker/.config/nvim" ] 
 then
   echo "NVIM config directory already existed; continuing..."
@@ -59,14 +66,15 @@ fi
 # Softlinking the NVIM config in this folder to the correct file system location
 # Note that the _entire_ file path is needed here - 
 # (should set up a bash variable to store the path to this location)
-ln -sf /Users/patrickspieker/Library/Mobile\ Documents/com~apple~CloudDocs/setup_scripts/vimrc_main ~/.config/nvim/init.vim
+ln -sf ./vimrc_main ~/.config/nvim/init.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # -------- END VIM CONFIG --------
 #
 # -------- VSCODE Config ---------
+# MAKE SURE VSCODE IS ALREADY INSTALLED
 # Soft linking the bashrc, then creating bash_profile such that it just references rc - all config should be in rc
-ln -sf /Users/patrickspieker/Library/Mobile\ Documents/com~apple~CloudDocs/setup_scripts/vscode_settings \
+ln -sf ./vscode_settings \
   ~/Library/Application\ Support/Code/User/settings.json
 # Appending with >> as opposed to overwriting with >
 echo "Finished configuing VS Code!"
